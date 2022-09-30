412b875758e04153790cee1bd1a99c32849c5e97
Vedantp53 <110757377+Vedantp53@users.noreply.github.com>
2022-09-30 16:48:03 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,33 @@
+
+
+<footer id="contactus" style="background-color: #272727;">
+    <div class="container" style="width: 100%;">
+				<div class="row justify-content-center">
+					<div class="col text-center">
+                        <img src="img/vi.png" style="width:60px;" alt="">
+                        <h2 class="footer-heading"> 
+                        <a href="index.php" style="color: #ffffff; font-family: 'Sora', sans-serif; font-size: 50px; text-decoration: none;"> Jobbers </a> 
+                        </h2>
+                    </div>	
+                                                
+                    <div class="menu text-center">
+                    <p>
+                    <a href="index.php" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> HOME </a> 
+                    <a href="#jbs" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> JOBS </a> 
+                    <a href="mailto: sumedh.patil@somaiya.edu" style="color: #ffd6a5; padding-top: 50px; font-family:Comfortaa;"> CONTACT </a>
+                    </p>
+                    </div>
+					
+				</div>
+				
+                <div class="row mt-5">
+					<div class="col-md-12 text-center">
+						<p class="copyrigt">
+                        <a style="color: #9FA4A9; text-decoration: none;"> All rights reserved 2022 | Developped by </a> 
+                        <a href="https://kjsieit.somaiya.edu.in/en" style="color: #FDFFAC;">KJSIEIT</a> 
+                        <a style="color: #FDFFAC; text-decoration: none;">& EXTC Dept.</a> 
+					    </p>
+					</div>
+				</div>
+    </div>
+</footer>
